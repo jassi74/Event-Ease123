c0cce66e5aa2c82120f317457b92972e589b84c8
jassi74 <167304101+jassi74@users.noreply.github.com>
2024-07-12 15:56:05 +0530

    changes revert

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 import BsState from './Context/BsState';
-import{BrowserRouter,Routes,Route} from 'react-router-dom';
+// import{BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './Pages/Home';
-import LoginPage from './LoginPage';
-import RegistrationPage from './RegistrationPage';
+// import LoginPage from './LoginPage';
+// import RegistrationPage from './RegistrationPage';
 
 function App() {
   return (
     <div className="App">
-     {/* {<BsState><Home/></BsState>} */}
+     {<BsState><Home/></BsState>}
        
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <Routes>
       <Route path='/'element={<LoginPage/>}/>
         <Route path='/Event-Ease123'element={<LoginPage/>}/>
@@ -19,7 +19,7 @@ function App() {
         <Route path='/home'element={<BsState><Home/></BsState>}/>
           <Route path='*'element={<div style={{color: "red", margin: "auto",textAlign:"center",fontSize:"40px"}}>Error 404</div>}/>
       </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
    
     </div>
   );
